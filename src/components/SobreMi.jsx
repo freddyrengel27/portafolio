ae13312b622f132c5546bf5badf6b288385ef27e
freddyrengel27 <freddy27rengel@gmail.com>
2022-05-22 18:56:31 -0400

    actualizacion build

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 
 import {FaHtml5, FaCss3Alt, FaReact, FaVuejs, FaNodeJs} from "react-icons/Fa";
-// import {SiExpress, SiJavascript} from "react-icons/Si"
+import {SiExpress, SiJavascript} from "react-icons/Si"
 import {DiMysql} from "react-icons/Di";
 import mi from "../assests/img/mi.jpg"
 
@@ -28,11 +28,11 @@ const SobreMi = () =>{
                         <div className="contenedor__skills">
                             <span className="skill"><FaHtml5 style={{color: "#E34C26", fontSize: "19px"}}/> HTML</span>
                             <span className="skill"><FaCss3Alt style={{color: "#264DE4", fontSize: "19px"}}/> CSS</span>
-                             <span className="skill">{/*<SiJavascript style={{color: "#F0DB4F", fontSize: "19px"}}/> */}JAVASCRIPT</span> 
+                             <span className="skill"><SiJavascript style={{color: "#F0DB4F", fontSize: "19px"}}/>JAVASCRIPT</span> 
                             <span className="skill"><FaReact style={{color: "#61DBFB", fontSize: "19px"}}/> REACT</span>
                             <span className="skill"><FaVuejs style={{color: "#41B883", fontSize: "19px"}}/> VUE</span>
                             <span className="skill"><FaNodeJs style={{color: "#3CB73A", fontSize: "19px"}}/> NODEJS</span>
-                            <span className="skill">{/*<SiExpress style={{color: "#B9B9B9", fontSize: "19px"}} />*/} EXPRESS</span>
+                            <span className="skill"><SiExpress style={{color: "#B9B9B9", fontSize: "19px"}} /> EXPRESS</span>
                             <span className="skill"><DiMysql style={{color: "#F29111", fontSize: "19px"}}/> MYSQL</span>
                         </div>
                     
